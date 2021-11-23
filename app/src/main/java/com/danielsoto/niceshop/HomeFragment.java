f5119fe5fe967f3a4e5ff43e4decdf99674e92a9
Daniel Soto <dasg.1993@gmail.com>
2021-11-23 14:33:15 -0500

    [feature] - setup action toobar in home fragment & create style for widget toolbar

--- app/src/main/java/com/danielsoto/niceshop/HomeFragment.java
+++ app/src/main/java/com/danielsoto/niceshop/HomeFragment.java
@@ -2,15 +2,31 @@ package com.danielsoto.niceshop;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        setUpToolbar(view);
+
+        return view;
+    }
+
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if(activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
     }
 }
